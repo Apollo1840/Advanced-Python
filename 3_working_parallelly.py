3ddf165b4291eb3251898ea305ab24002dd9f7a2
Apollo1840 <zoucongyu1993@hotmail.com>
2018-09-23 17:20:09 +0200

    chicken example

--- 3_working_parallelly.py
+++ 3_working_parallelly.py
@@ -38,3 +38,100 @@ for t in thread_list:
 
 print('it takes {} seconds'.format(time.time()-t0))    
 print('hello world')
+
+
+
+
+
+# chicken lay egg example:
+import random
+
+eggs = []
+
+class chicken():
+    sound = 'koko\n'
+    
+    def lay_egg(self):
+        time.sleep(random.choice([1, 1.1, 1.2, 1.3, 10]))
+        print(self.sound)
+        eggs.append(1)
+
+
+def basic_process(): 
+    ck0 = chicken()
+    while(len(eggs) < 12):
+        ck0.lay_egg()
+    
+
+def three_chicken():
+    ck0 = chicken()
+    ck1 = chicken()
+    ck2 = chicken()
+    
+    while(len(eggs) < 12):
+        ck0.lay_egg()
+        ck1.lay_egg()
+        ck2.lay_egg()
+        
+
+def three_chicken_parallel():
+    ck0 = chicken()
+    ck1 = chicken()
+    ck2 = chicken()
+    
+    thread_list=[]
+    
+    for epoch in range(4):
+        for i in range(3):
+            ck = eval('ck{}'.format(i))
+            t = threading.Thread(target=ck.lay_egg)
+            thread_list.append(t)
+            t.start()
+        for t in thread_list:
+            t.join()
+
+def three_chicken_parallel_2():
+    thread_list=[]
+    
+    for epoch in range(4):
+        for i in range(3):
+            t = threading.Thread(target=chicken().lay_egg)
+            thread_list.append(t)
+            t.start()
+        for t in thread_list:
+            t.join()
+        
+        chicken.sound = chicken.sound[:-1] + '!\n'
+
+def lag4eggs():
+    ck = chicken()
+    for i in range(4):
+        ck.lay_egg()
+    chicken.sound = chicken.sound[:-1] + '!\n'
+            
+def three_chicken_parallel_3():
+    thread_list=[]
+
+    for i in range(3):
+        t = threading.Thread(target=lag4eggs)
+        thread_list.append(t)
+        t.start()
+        
+    for t in thread_list:
+        t.join()
+        
+        
+        
+def test():
+    t0 = time.time()
+    three_chicken_parallel_3()
+    print('spend {}s'.format(time.time() - t0))
+
+
+
+
+
+
+    
+    
+        
